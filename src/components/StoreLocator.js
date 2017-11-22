a01d149a2ef79cadb7b9cf5b2861e686ce23c1b4
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2017-11-22 15:32:13 +0300

    - render map - add markers on a map - add info windows for each shop marker

--- src/components/StoreLocator.js
+++ src/components/StoreLocator.js
@@ -3,30 +3,69 @@ import {loadScript} from 'lib/utils';
 import classNames from './StoreLocator.css';
 
 class StoreLocator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isMapLoaded: Boolean(window.google && window.google.maps)
-    };
-  }
+  static defaultProps = {
+    stores: [],
+    zoom: 6,
+    center: {lat: 39.6433995, lng: -6.4396778}
+  };
 
   loadGoogleMaps() {
-    if (this.state.isMapLoaded) return;
-    loadScript(
+    if (window.google && window.google.maps) return Promise.resolve();
+    return loadScript(
       `https://maps.googleapis.com/maps/api/js?key=${this.props.apiKey}&libraries=geometry`
-    ).then(() => {
-      this.setState({isMapLoaded: true});
-    });
+    );
   }
 
+  centerOnUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        p => {
+          this.map.setCenter(new google.maps.LatLng(p.coords.latitude, p.coords.longitude));
+        },
+        () => {
+          throw new Error('user denied request for position');
+        }
+      );
+    } else {
+      throw new Error('no geolocation support');
+    }
+  }
+
+  addStoreMarker = store => {
+    const infowindow = new google.maps.InfoWindow({
+      content: `<div class="${classNames.infoWindow}">
+          <h4>${store.title}</h4>
+          ${store.details}
+        </div>`
+    });
+    const marker = new google.maps.Marker({
+      position: store.position,
+      map: this.map,
+      title: store.title
+    });
+    marker.addListener('click', function() {
+      infowindow.open(this.map, marker);
+    });
+  };
+
+  constructMap = () => {
+    const {center, zoom} = this.props;
+    this.map = new window.google.maps.Map(this.mapFrame, {
+      center,
+      zoom
+    });
+    this.centerOnUserLocation();
+    this.props.stores.forEach(this.addStoreMarker);
+  };
+
   componentDidMount() {
-    this.loadGoogleMaps();
+    this.loadGoogleMaps().then(this.constructMap);
   }
 
   render() {
     return (
       <div className={classNames.container}>
-        <div className={classNames.map} ref={map => (this.map = map)} />
+        <div className={classNames.map} ref={mapFrame => (this.mapFrame = mapFrame)} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import {render} from 'preact';
 import StoreLocator from 'components/StoreLocator';
 
-module.exports = function({container = 'store-locator', stores = [], apiKey}) {
-  render(<StoreLocator stores={stores} apiKey={apiKey} />, document.getElementById(container));
+module.exports = function({container, stores, apiKey, zoom, defaultCenter}) {
+  render(
+    <StoreLocator stores={stores} apiKey={apiKey} zoom={zoom} defaultCenter={defaultCenter} />,
+    document.getElementById(container)
+  );
 };
